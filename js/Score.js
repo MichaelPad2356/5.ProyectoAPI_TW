6e5231ba570cbddbb5a043d24f7bf0b995b148f1
AketzaliCastorena <aketzalicastorena18@gmail.com>
2024-03-09 22:33:04 -0600

    cambios

--- js/Score.js
+++ js/Score.js
@@ -11,6 +11,12 @@ function PlayAudio(){
     document.getElementById('musicaFondo').play();
 }
 
+function PlayAudio() {
+    var audio = document.getElementById('musicaFondo');
+    audio.volume = 0.1; // Establece el volumen al 50%
+    audio.play();
+}
+
 function endGame(alias, score, time) {
     // Mostrar la pantalla de felicitación
     document.getElementById('gameCanvas').style.display = 'none';
