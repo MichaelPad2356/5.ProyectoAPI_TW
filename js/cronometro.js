0acb4285b3dd446c5c651156b6038fe6d404d203
paniniL8 <al291682@edu.uaa.mx>
2024-03-10 13:36:23 -0600

    diseño para el cronometro

--- js/cronometro.js
+++ js/cronometro.js
@@ -6,6 +6,15 @@ let tiempoInicial = 0; // Tiempo inicial en segundos
 // Función para iniciar el cronómetro
 function iniciarCronometro() {
     let contador = document.getElementById('cronometro');
+    contador.style.position = 'absolute';
+    contador.style.top = '20px'; // Ajusta la posición vertical del cronómetro
+    contador.style.right = '450px'; // Ajusta la posición horizontal del cronómetro para que esté a la derecha
+    contador.style.fontSize = '18px'; // Ajusta el tamaño de la fuente del cronómetro
+    contador.style.color = 'white'; // Cambia el color del texto a blanco
+    contador.style.background = '#0047ab'; // Cambia el fondo a un azul fuerte
+    contador.style.padding = '10px'; // Añade relleno alrededor del texto
+    contador.style.borderRadius = '10px'; // Añade bordes redondeados
+    contador.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.5)'; // Agrega sombra al texto para mejorar la legibilidad
 
     // Función que actualiza el cronómetro cada segundo
     intervalo = setInterval(() => {
